f818a649ca3a3e7a50270f7bd9c6919d09701005
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-10-03 14:02:24 -0400

    88. development dependencies

--- intro-express/server.js
+++ intro-express/server.js
@@ -4,11 +4,29 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein',
+  },
+  {
     id: 1,
     name: 'Sir Isaac Newton',
-  });
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:id', (req, res) => {
+  const { id } = Number(req.params);
+  const friend = friends[id];
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(404).json({ error: 'Friend does not exist' });
+  }
 });
 
 app.get('/messages', (req, res) => {
