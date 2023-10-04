3db161a7d638370f79f37aa43be51dc1c5d58bae
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-10-04 12:37:09 -0400

    100. layouts and separation of concerns

--- intro-express/controllers/messages.controller.js
+++ intro-express/controllers/messages.controller.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const getMessages = (req, res) => {
-  res.sendFile(
-    path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg')
-  );
-  // return res.send('<ul><li>Hello Albert!</li></ul');
+  res.render('messages', {
+    title: 'Messages to my friends!',
+    friend: 'Elon Musk',
+  });
 };
 
 const postMessage = (req, res) => {
