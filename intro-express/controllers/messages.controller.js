72af8339374eeb22daa192448853a144efce48b0
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-10-04 10:38:09 -0400

    97. sending files

--- intro-express/controllers/messages.controller.js
+++ intro-express/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const getMessages = (req, res) => {
-  return res.send('<ul><li>Hello Albert!</li></ul');
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+  // return res.send('<ul><li>Hello Albert!</li></ul');
 };
 
 const postMessage = (req, res) => {
