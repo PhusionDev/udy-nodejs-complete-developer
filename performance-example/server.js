c2dd07d3a100bd7bcac16bd38802dc2f3a8fadfd
Kavabux <PhusionDev@gmail.com>
2023-10-08 13:56:35 -0400

    143. maximizing cluster performance

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -10,14 +12,31 @@ const delay = (duration) => {
 };
 
 app.get('/', (req, res) => {
-  res.send('Performance Example');
+  // BLOCKING FUNCTIONS
+  // JSON.stringify();
+  // JSON.parse();
+  // [5,1,2,3,4].sort();
+
+  res.send(`Performance Example - ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! - ${process.pid}`);
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+// console.log('Running server.js');
+
+if (cluster.isMaster) {
+  console.log('Master has been started...');
+  const cpus = os.cpus().length;
+  console.log(`Forking for ${cpus} CPUs`);
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Worker has been started...');
+  app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+  });
+}
